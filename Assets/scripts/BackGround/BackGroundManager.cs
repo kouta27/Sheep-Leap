5beb0a5ecb17d2ed40e502c39db88f897cb4618e
parasu34 <parasakisu3888@gmail.com>
2025-10-01 22:56:43 +0900

    背景の処理とマテリアルの生成

---
+++ Assets/scripts/BackGround/BackGroundManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class BackGroundManager : MonoBehaviour
+{
+    [SerializeField]
+    Material[] materials;
+    float scrollSpeed = 0.1f;
+    float scrollAcceleration = 0.1f;
+    float scrollTime = 0;
+    bool isEnded = false;
+    public void StartScroll()
+    {
+        StartCoroutine(Scroll());
+    }
+    public void EndScroll()
+    {
+        isEnded = true;
+    }
+
+    IEnumerator Scroll()
+    {
+        float currentSpeed = 0;
+        while (!isEnded)
+        {
+            if (currentSpeed < scrollSpeed)
+            {
+                currentSpeed += scrollAcceleration * Time.deltaTime;
+                scrollTime += currentSpeed *  Time.deltaTime;
+            }
+            else
+            {
+                scrollTime += scrollSpeed *Time.deltaTime;
+            }
+            foreach (var mat in materials)
+            {
+                mat.SetFloat("_ScrollTime",scrollTime );
+            }
+            yield return null;
+        }
+        while (currentSpeed > 0)
+        {
+            scrollTime += currentSpeed * Time.deltaTime;
+            foreach (var mat in materials)
+            {
+                mat.SetFloat("_ScrollTime", scrollTime);
+            }
+            currentSpeed -= scrollAcceleration * Time.deltaTime;
+            yield return null;
+        }
+        isEnded = false;
+    }
+
+}
